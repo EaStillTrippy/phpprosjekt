a01becfcf24690d98875090f9ac90bb34bb0a945
Erik-Andre Ørn <ea.oern@gmail.com>
2020-05-14 16:59:12 +0200

    Update loggut.php

--- loggut.php
+++ loggut.php
@@ -3,5 +3,4 @@ session_start();
 session_destroy();
 print("<meta http-equiv='refresh' content='0;url=default.php'>");
 ?>
-<!-- Denne siden er utviklet av Tom Noodt, siste gang endret 02.12.2018 -->
-<!-- Denne siden er kontrollert av Erik-Andre Ørn, siste gang 03.12.2018 -->
+
